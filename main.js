29b46770807129209f3acb36a62280a943bde828
Rangelito1 <75523966+Rangelito1@users.noreply.github.com>
2024-06-04 21:35:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-import { ShardEvents, ShardingManager } from 'discord.js';
+import { ShardingManager } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
+// Cambia la referencia de './bot.js' a './commands/main/Inventory.js'
+const manager = new ShardingManager('./commands/main/Inventory.js', { token: process.env.TOKEN });
 
-const readyShards = new Set();
 manager.on('shardCreate', shard => {
     console.log(`Launched shard ${shard.id}`);
-    shard.on(ShardEvents.Ready, async () => {
+    shard.on('ready', () => {
         console.log(`Shard ${shard.id} ready`);
-        readyShards.add(shard.id);
-        if(readyShards.size === manager.totalShards) {
-            console.log(`All ${manager.totalShards} shards ready`);
-            await manager.broadcastEval(c => c.emit('allShardsReady'));
-        }
     });
 });
-await manager.spawn();
+
+manager.spawn();
